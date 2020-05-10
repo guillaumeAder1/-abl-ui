2ed6a13a98bb003d31f6b51c102fa15a6154bd19
guillaume.ader <guillaume.ader@visma.com>
2020-05-10 18:38:16 +0200

    config

--- packages/atoms/src/button.js
+++ packages/atoms/src/button.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import styled from "@emotion/styled";
 
-const Button = () => (<button>click me</button>);
+const Wrapper = styled.button`
+  text-transform: uppercase;
+  font-size: 1.5em;
+  font-weight: bold;
+  letter-spacing: 4px;
+  background: #5cdb95;
+  color: #05385b;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+`;
+const Button = () => {
+  const handleClick = () => alert('okokook');
+  return (
+    <Wrapper onClick={handleClick} >click me</Wrapper>
+  )
+}
 
 export default Button;
