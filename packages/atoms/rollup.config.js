cc1028b45fc9238864a09a287c4f760a95cd609d
guillaume.ader <guillaume.ader@visma.com>
2020-05-11 22:00:41 +0200

    feat: config basic rollup

--- packages/atoms/rollup.config.js
+++ packages/atoms/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
 
 
 import packageJSON from './package.json';
@@ -18,6 +19,7 @@ export default [
 			babel({
 				exclude: 'node_modules/**'
 			}),
+			external(),
 			resolve(),
 			commonjs(),
 		]
