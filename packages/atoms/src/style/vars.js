bdc1a165753519d29a0255af78f912f440bb8f4c
guillaume.ader <guillaume.ader@visma.com>
2020-05-11 20:11:13 +0200

    chore: reorganiser structure

--- packages/atoms/src/style/vars.js
+++ packages/atoms/src/style/vars.js
@@ -1,5 +1,7 @@
-export const font = {
+import { css } from '@emotion/core';
+
+export const font = css`
 	fontFamily: 'helvetica, arial',
 	fontSize: 12,
 	color: '#333333',
-};
+`;
--- packages/atoms/tests/button.test.js
+++ packages/atoms/tests/button.test.js
@@ -1,6 +1,7 @@
 import { render, getByText } from '@testing-library/react';
 import React from 'react';
-import Button from 'button';
+import Button from 'components/button';
+
 
 describe('Button', () => {
 	test('should display text', () => {
