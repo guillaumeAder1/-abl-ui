20f6e9ce877ce8fd38125da45277244a03b362d0
guillaume.ader <guillaume.ader@visma.com>
2020-05-11 20:00:43 +0200

    create basic button style & const var

--- packages/atoms/src/button.js
+++ packages/atoms/src/button.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { font } from './style/vars';
+
 
 const Wrapper = styled.button`
-  text-transform: uppercase;
-  font-size: 1.5em;
-  font-weight: bold;
-  letter-spacing: 4px;
-  background: #5cdb95;
-  color: #05385b;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
+  ${font}
+  padding: 1px 2px;
+  border-color: #333333;
+  background-color: lightgray;
+  :hover {
+    background-color: orange;
+  }
 `;
 const Button = props => {
-	const handleClick = () => alert('okokook');
+	const handleClick = () => {};
 	return (
 		<Wrapper onClick={handleClick} >{props.text}</Wrapper>
 	);
---
+++ packages/atoms/src/style/vars.js
@@ -0,0 +1,5 @@
+export const font = {
+	fontFamily: 'helvetica, arial',
+	fontSize: 12,
+	color: '#333333',
+};
