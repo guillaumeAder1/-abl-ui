baea79a2489f0222b083591dcb370665be1ffdce
guillaume.ader <guillaume.ader@visma.com>
2020-05-23 18:23:52 +0200

    test: add test for buildTicks utils

--- packages/atoms/src/components/VolumeSlider/tests/utils.test.js
+++ packages/atoms/src/components/VolumeSlider/tests/utils.test.js
@@ -1,4 +1,4 @@
-import { pixelToValue, valueToPixel } from '../utils';
+import { pixelToValue, valueToPixel, buildTicks } from '../utils';
 
 describe('Utils.js', () => {
 	test('pixelToValue', () => {
@@ -7,5 +7,10 @@ describe('Utils.js', () => {
 	});
 	test('valueToPixel', () => {
 		expect(valueToPixel(75, 100, 150)).toBe(50);
+	});
+	test('buildTicks should return correct values', () => {
+		expect(buildTicks(3)).toEqual(['long', 'short', 'long']);
+		expect(buildTicks(4)).toEqual(['long', 'short', 'short', 'long']);
+		expect(buildTicks(2)).toEqual(['long', 'long']);
 	});
 });
