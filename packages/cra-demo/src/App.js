4de645ce4a308ecc278af2b7f02bdc57b2cc9e1c
guillaume.ader <guillaume.ader@visma.com>
2020-05-26 19:20:48 +0200

    chore: install

--- packages/cra-demo/src/App.js
+++ packages/cra-demo/src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import { Button } from '@abl/atoms';
+import { Button, Slider } from '@abl/atoms';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-				<Button text='okoko' />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Button />
+				<Slider />
+			</header>
+		</div>
+	);
 }
 
 export default App;
