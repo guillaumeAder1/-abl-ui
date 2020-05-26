d7c2ce80da8e17b120fd47155b82938c418dccb8
guillaume.ader <guillaume.ader@visma.com>
2020-05-26 22:23:19 +0200

    feat: test hook on button component

--- packages/atoms/src/components/button.js
+++ packages/atoms/src/components/button.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { font, button } from 'style/vars';
 
@@ -7,6 +7,7 @@ const Wrapper = styled.button`
 	${button}
 `;
 const Button = ({ children, ...props }) => {
+	const [val, setVal] = useState(0);
 	const handleClick = () => {};
 	return <Wrapper onClick={handleClick}>{children}</Wrapper>;
 };
