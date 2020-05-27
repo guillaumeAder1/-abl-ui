afcb2a17a4ad481ae5c7a652326ddc5679ba52cb
guillaume.ader <guillaume.ader@visma.com>
2020-05-27 09:35:28 +0200

    fix: import name error

--- packages/atoms/src/components/Block.js
+++ packages/atoms/src/components/Block.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from './Button';
+import Button from './button';
 import Select from './Select';
 import { Slider } from './VolumeSlider';
 import styled from '@emotion/styled';
