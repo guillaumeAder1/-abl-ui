2ebabe0fb6bf9e78da75a16edf95ab7f7d2bde8b
guillaume.ader <guillaume.ader@visma.com>
2020-05-28 16:37:53 +0200

    feat: add story and draw basic circle with stroke

--- packages/atoms/src/components/VolumeKnob/knob.js
+++ packages/atoms/src/components/VolumeKnob/knob.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
-const Knob = ({...props}) => {
-	return (  );
-}
- 
+const SvgContainer = styled('svg')`
+	height: 100px;
+	width: 100px;
+	fill: gray;
+`;
+const Circle = styled('circle')`
+	cy: 50px;
+	cx: 50px;
+	r: 50px;
+	stroke: black;
+	fill: transparent;
+	stroke-width: 5px;
+`;
+const Knob = ({ ...props }) => {
+	const test = Math.round(0.2);
+	return (
+		<div>
+			<SvgContainer>
+				<Circle />
+			</SvgContainer>
+		</div>
+	);
+};
+
 export default Knob;
--- packages/atoms/stories/knobs.stories.js
+++ packages/atoms/stories/knobs.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slider } from 'components/VolumeSlider';
+import { Knob } from 'components/VolumeKnob';
 
 export default {
 	title: '@abl/Knobs',
@@ -12,5 +13,8 @@ export const slider = () => (
 	<div>
 		<h1>Slider</h1>
 		<Slider />
+
+		<h1>Knob</h1>
+		<Knob />
 	</div>
 );
