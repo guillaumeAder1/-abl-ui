50ae6c213af716ce0b40d01f6d14c6ad0aff5f01
guillaume.ader <guillaume.ader@visma.com>
2020-05-28 16:20:33 +0200

    feat: start working on new volume knob component

---
+++ packages/atoms/src/components/VolumeKnob/container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Knob from './knob';
+
+const Container = () => {
+	return (
+		<div>
+			<Knob />
+		</div>
+	);
+};
+
+export default Container;
---
+++ packages/atoms/src/components/VolumeKnob/knob.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Knob = ({...props}) => {
+	return (  );
+}
+ 
+export default Knob;
