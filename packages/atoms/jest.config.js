ff47f4511a69689de0315a8bdf9a5e2813ffe45c
guillaume.ader <guillaume.ader@visma.com>
2020-06-25 11:22:26 +0200

    chore: start adding jest config

---
+++ packages/atoms/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	modulePaths: ['<rootDir>/src/components'],
+	setupFilesAfterEnv: ['<rootDir>/tests/setup.jest.js'],
+};
---
+++ packages/atoms/src/components/VolumeKnob/tests/container.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Container from '../container';
+
+describe('Volume Knob Container', () => {
+	test('should display label and output value', () => {
+		render(<Container />);
+		screen.getByRole('slider');
+		screen.getByText(/setting/i);
+		expect(screen.findByText('setting')).toBeInTheDocument();
+		screen.debug();
+	});
+});
