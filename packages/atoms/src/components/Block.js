8dc7f3d1f6a4079ea339040a854a48e8cbd4f8c5
Guillaume Ader <guillaumeader@Guillaumes-MacBook-Pro.local>
2021-02-17 17:36:04 +0000

    feat: fix naming path issue

--- packages/atoms/src/components/Block.js
+++ packages/atoms/src/components/Block.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from './button';
-import Select from './Select';
+import Select from './select';
 import { Slider } from './VolumeSlider';
 import styled from '@emotion/styled';
 import { baseBlock } from 'style/vars';
